eb3ba247887a53f7805645d9a0c749f667961437
Drew Wetherington <dgwetherington@gmail.com>
2024-12-17 01:07:12 -0600

    Add some git ignore

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -12,6 +12,9 @@
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 
+// httplib.h has dependency that refined byte 
+#define _HAS_STD_BYTE 0
+
 #include <iostream>
 #include <fstream>
 #include "Monster.h"
@@ -25,8 +28,21 @@
 #include "Person.h"
 #include "StringFunctions.cpp"
 #include "Mammal.h"
+#include "MultithreadExample.h"
+#include "httplib.h"
+#include <algorithm>
 
 using std::array, std::unique_ptr;
+using namespace std::chrono_literals;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
+
+void RunHttpServer();
+string MakeGetRequest(string url);
+void TestRequest();
 
 int main()
 {
@@ -56,11 +72,27 @@ int main()
     Log(fileInfo.filePath);
 
 
-  
     // String manipulation
     auto strVector = std::vector<string> { "Hello", "World", "Wasuupppp"};
     auto joined_str = join(strVector.begin(), strVector.end(), "___");
     Log("String output: " + joined_str);
+
+
+    std::array<string, 5> intArr = {"A", "AB", "ABC"};
+    int idx = 0;
+    //const auto* pAddressOfItemZero = &(*intArr.begin());
+
+    for (auto& ele: intArr) {
+        //const auto* pAddressOfElement = &ele;
+
+        printf("Address of element %i is %p\n", idx, &ele);
+        idx++;
+    }
+
+    printf("Address of String is %p\n", &"HELLO");
+    //auto str1 = strstr("DEFABC", "ABC");
+    //Log(str1);
+
 
     // Map<string, int>
     // Map<string, Struct or class>
@@ -116,9 +148,54 @@ int main()
     // 1. JSON serialization and deserialization to structs. (need reflection or external library).
     // 2. Multithreaded work on an array of 1M strings. Possibly sort the characters in the strings alphabetically.
     // 3. Implement interface (abstract class). Multiple interfaces?
+
+    startThreads();
+
+    thread tHttpServ(RunHttpServer);
+    tHttpServ.detach();
+    //tHttpServ.join();
+
+
+
+    Log("Press enter to make GET Request");
+    if (cin.get()) {
+        TestRequest();
+        TestRequest();
+        TestRequest();
+    }
+
     Log("************* COMPLETED *************");
 }
 
+void TestRequest() {
+    auto t1 = high_resolution_clock::now();
+
+    thread r3(MakeGetRequest, "127.0.0.1:8080");
+    r3.join();
+
+    auto t2 = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as an integer. */
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    duration<double, std::milli> ms_double = t2 - t1;
+
+    Log("Request time: " + to_string(ms_double.count()) + "ms");
+}
+
+void RunHttpServer() {
+    // HTTP
+    httplib::Server svr;
+    // HTTPS
+    //httplib::SSLServer svr;
+    svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
+        Log("Loading /");
+        res.set_content("Hello World from Server :8080!", "text/plain");
+        });
+
+    Log("Running on 127.0.0.1:8080");
+
+    svr.listen("127.0.0.1", 8080);
+}
 
 // This is why Monster sayName function needs to be virtual.
 // The virtual keyword introduces Late-binding to execute the most-specific type's defined function.
@@ -126,6 +203,17 @@ void CallFunctionOnMonster(Monster* monster) {
     monster->sayName();
 }
 
+
+string MakeGetRequest(string url) {
+    // HTTPS
+    httplib::Client cli(url);
+
+    auto res = cli.Get("/");
+    string code = to_string(res->status);
+    Log("[GET] " + url + " " + code);
+    
+    return res->body;
+}
 
 void PrintOutPointerTest()
 {
@@ -156,6 +244,4 @@ void PrintOutPointerTest()
     unique_ptr<Mammal> mammalPtr = make_unique<Mammal>("MAMMAL NAME");
     mammalPtr->sayName();
     CallFunctionOnMonster(mammalPtr.get());
-
-
 }
